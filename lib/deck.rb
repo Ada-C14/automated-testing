984dac934a8c64384e02554eaf9ad7896b254a31
Alice D <alicedilanchian@gmail.com>
2020-09-29 11:26:40 -0500

    Wave 3 - predominantly from following the video

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,14 +6,24 @@ require_relative 'card'
 class Deck
 
   def initialize
+    @cards = []
+    [:hearts, :spades, :clubs, :diamonds].each do |suit|
+      (1..13).each do |value|
+        @cards << Card.new(value, suit)
+        end
+      end
 
   end
 
   def draw
     # returns a card
+    # .pop removes the last element from self and returns it, or nil if the array is empty.
+    drawn_card = @cards.pop
+    return drawn_card
   end
 
   def shuffle
     # shuffles the deck
+    @cards.shuffle
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,9 +1,43 @@
 require_relative 'test_helper'
 
 describe Deck do
-  # Write tests for the Deck class.
-  # What should you test?
-    # 1.  Expected Behavior
-    # 2.  Edge cases
+  before do
+    @deck = Deck.new
+  end
+
+  describe "initialize" do
+    it "can be instantiated" do
+      expect(@deck).must_be_instance_of Deck
+    end
+  end
+
+  describe "draw method" do
+    it "must respond to draw method" do
+      expect(@deck).must_respond_to :draw
+    end
+
+    it "returns a card" do
+      expect(@deck.draw).must_be_instance_of Card
+    end
+
+    it "will let me draw 52 cards from an initial deck" do
+      52.times do
+        expect(@deck.draw).must_be_instance_of Card
+      end
+    end
+
+    it "will return nil for an empty deck" do
+      52.times do
+        expect(@deck.draw).must_be_instance_of Card
+      end
+      expect(@deck.draw).must_be_nil
+    end
+  end
+
+  describe "shuffle" do
+    it "responds to shuffle" do
+      expect(@deck).must_respond_to :shuffle
+    end
+  end
 
 end
