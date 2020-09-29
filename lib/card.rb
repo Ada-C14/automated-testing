333c7276b8097cdac16bb9e8ec59fca2ce7a5edf
Alice D <alicedilanchian@gmail.com>
2020-09-29 10:33:38 -0500

    Wave 1

--- lib/card.rb
+++ lib/card.rb
@@ -1,4 +1,3 @@
-
 # card.rb
 
 class Card
@@ -7,8 +6,13 @@ class Card
   def initialize(value, suit)
     @value = value
     @suit = suit
-    
 
+    valid_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    valid_suits = [:spades, :diamonds, :hearts, :clubs]
+
+    if (valid_values.include?(@value) == false) || (valid_suits.include?(@suit) == false)
+      raise ArgumentError.new
+    end
   end
 
   def to_s
